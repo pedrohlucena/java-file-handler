e41aaf9387f653f534f84cfefb69dd867653ae53
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-10-02 14:06:46 -0300

    add showFileInfo function

--- java-file-handler/src/business/FileHandler.java
+++ java-file-handler/src/business/FileHandler.java
@@ -1,5 +1,6 @@
 package business;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,14 +8,14 @@ import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.List;
 
+import exceptions.FileDoesNotExistsException;
+import exceptions.IsADirectoryException;
 import utils.HandleError;
 
 public class FileHandler implements Serializable {
 	private static final long serialVersionUID = 1L;
-
-	public FileHandler() {}
 	
-	public void write(String fileName, String path, List<String> content) {
+	public static void write(String fileName, String path, List<String> content) {
 		try (PrintWriter print = new PrintWriter(new FileWriter(path))) {
 			for(String row: content) {
 				print.println(row);
@@ -26,7 +27,7 @@ public class FileHandler implements Serializable {
 		}
 	}
 	
-	public void read(String path) {
+	public static void read(String path) {
 		try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
 		
 			String linha = reader.readLine();
@@ -40,6 +41,55 @@ public class FileHandler implements Serializable {
 		}
 	}
 	
-	// mostrar informações sobre um arquivo
-	// mostrar informações sobre pastas
+	public static void showFileInfo(String path) {
+		File file = new File(path);
+		try {
+			if(!file.exists()) { throw new FileDoesNotExistsException("O arquivo informado não existe!"); }
+			if(file.isDirectory()) { throw new IsADirectoryException("O path informado é de uma pasta e não de um arquivo."); }
+		
+			String message = (
+					file.getName() + "\n" +
+					file.getPath() + "\n" +
+					"O arquivo pode ser lido? " + file.canRead() + "\n" +
+					"O arquivo pode ser gravado? " + file.canWrite() + "\n" +
+					"Tamanho do arquivo: " + file.length()
+			)
+					.replace("true", "Sim")
+					.replace("false", "Não");
+			
+			System.out.print(message);
+		} catch (FileDoesNotExistsException e) {
+			HandleError.handleError(e);
+		} catch (IsADirectoryException e) {
+			HandleError.handleError(e);
+		} catch (Exception e) {
+			HandleError.handleError(e);
+		}
+	}
+	
+	public static void showDirectoryInfo(String path) {
+		File file = new File(path);
+		try {
+			if(!file.exists()) { throw new FileDoesNotExistsException("O arquivo informado não existe!"); }
+			if(file.isDirectory()) { throw new IsADirectoryException("O path informado é de uma pasta e não de um arquivo."); }
+		
+			String message = (
+					file.getName() + "\n" +
+					file.getPath() + "\n" +
+					"O arquivo pode ser lido? " + file.canRead() + "\n" +
+					"O arquivo pode ser gravado? " + file.canWrite() + "\n" +
+					"Tamanho do arquivo: " + file.length()
+			)
+					.replace("true", "Sim")
+					.replace("false", "Não");
+			
+			System.out.print(message);
+		} catch (FileDoesNotExistsException e) {
+			HandleError.handleError(e);
+		} catch (IsADirectoryException e) {
+			HandleError.handleError(e);
+		} catch (Exception e) {
+			HandleError.handleError(e);
+		}
+	}
 }
